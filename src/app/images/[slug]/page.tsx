f59842a1c4f8669e758d86d6c83ff8c11866ab9e
danitest45 <47956492+danitest45@users.noreply.github.com>
2025-10-27 20:39:44 -0300

    Simplify image model page params typing

--- src/app/images/[slug]/page.tsx
+++ src/app/images/[slug]/page.tsx
@@ -1,36 +1,12 @@
 import ImageModelClient from './ImageModelClient';
 
-type SegmentParams = Record<string, string | string[] | undefined>;
+type ImageModelPageParams = { slug: string };
 
-type PageProps = {
-  params?: Promise<SegmentParams>;
-  searchParams?: Promise<Record<string, string | string[] | undefined>>;
-};
-
-function isPromise<T>(value: unknown): value is Promise<T> {
-  return (
-    typeof value === 'object' &&
-    value !== null &&
-    typeof (value as Promise<T>).then === 'function'
-  );
-}
-
-function assertHasSlug(params: SegmentParams): asserts params is { slug: string } {
-  if (typeof params.slug !== 'string') {
-    throw new Error('Missing slug param');
-  }
-}
-
-export default function ImageModelPage({ params }: PageProps) {
-  if (!params) {
-    throw new Error('Missing route params');
-  }
-
-  if (isPromise<SegmentParams>(params)) {
-    throw new Error('Route params must be provided synchronously');
-  }
-
-  assertHasSlug(params);
-
-  return <ImageModelClient slug={params.slug} />;
+export default function ImageModelPage({
+  params,
+}: {
+  params: ImageModelPageParams;
+}) {
+  const { slug } = params;
+  return <ImageModelClient slug={slug} />;
 }
